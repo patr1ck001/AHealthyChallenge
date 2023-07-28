e731849f6566525b4b0cf69ffe9b89b705edc4af
patr1ck001 <patrick.niantcho@gmail.com>
2023-07-28 17:58:47 +0200

    implement basic LeaderBoardScreenDetails

--- app/src/main/java/com/example/ahealthychallenge/presentation/screen/welcomeScreen/leaderBoadScreenDetails/LeaderBoardScreenDetails.kt
+++ app/src/main/java/com/example/ahealthychallenge/presentation/screen/welcomeScreen/leaderBoadScreenDetails/LeaderBoardScreenDetails.kt
@@ -1,12 +1,44 @@
 package com.example.ahealthychallenge.presentation.screen.welcomeScreen.leaderBoadScreenDetails
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.ahealthychallenge.R
 import com.example.ahealthychallenge.data.UserPointsSheet
-
+import com.example.ahealthychallenge.presentation.component.sessionDetailsItem
 
 @Composable
-fun LeaderBoardScreenDetails (
+fun LeaderBoardScreenDetails(
     userPointsSheet: UserPointsSheet
 ) {
-    Text(text = "walking: ${userPointsSheet.pointsWalking} \n running: ${userPointsSheet.pointRunning} \n total ${userPointsSheet.totalPoints} \n" )
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        sessionDetailsItem(labelId = R.string.points_walking) {
+            Text("${userPointsSheet.pointsWalking}")
+        }
+
+        sessionDetailsItem(labelId = R.string.points_running) {
+            Text("${userPointsSheet.pointRunning}")
+        }
+
+        sessionDetailsItem(labelId = R.string.points_biking) {
+            Text("${userPointsSheet.pointsCycling}")
+        }
+
+        sessionDetailsItem(labelId = R.string.points_working_out) {
+            Text("${userPointsSheet.pointsWorkout}")
+        }
+
+        sessionDetailsItem(labelId = R.string.total_points) {
+            Text("${userPointsSheet.totalPoints}")
+        }
+    }
 }
